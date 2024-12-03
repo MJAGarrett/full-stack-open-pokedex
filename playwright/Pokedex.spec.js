74fc9d746b37d75b1160c663a9c85cea5bf75dda
Michael Garrett <michaeljgarrett@outlook.com>
2024-12-02 20:53:13 -0800

    Adds simple e2e test

--- playwright/Pokedex.spec.js
+++ playwright/Pokedex.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('a specific pokemon\'s page can be navigated to', async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('ivysaur').click()
+    await page.waitForURL('/pokemon/ivysaur') // Wait for page to transition to some pokemon's page
+
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
